494737a8ff127aeb2aa363add237fb639611c579
saied83 <saiedtechit@gmail.com>
2024-03-01 15:31:44 +0600

    debug countOccurenceBS file

--- Divide & Conquer/Search Algorithm/BinarySearch/countOccurrenceBS.py
+++ Divide & Conquer/Search Algorithm/BinarySearch/countOccurrenceBS.py
@@ -2,11 +2,14 @@ import findOccurrenceBS as fbs
 
 def main():
     arr = [1,2, 3, 4, 5, 5, 5, 5, 5, 5, 7, 89, 89, 96, 100]
-    target = int(input("Number you want to search: "))
-    firstOccurrence = fbs.findOccurrence(arr, 5, True)
-    lastOccurrence = fbs.findOccurrence(arr, 5, False)
-    count = lastOccurrence-firstOccurrence+1
-    print(count)
+    t = int(input("Number you want to search: "))
+    firstOccurrence = fbs.findOccurrence(arr, t, True)
+    if firstOccurrence == -1:
+        print(0)
+    else:
+        lastOccurrence = fbs.findOccurrence(arr, t, False)
+        count = lastOccurrence-firstOccurrence+1
+        print(count)
 
 if __name__ =="__main__":
     main()
